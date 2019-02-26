e020fa1503c35e166ce466a7f5db33ef5e6cb8c8
houfeng723 <32111732+houfeng723@users.noreply.github.com>
2019-02-25 20:00:49 -0600

    "finish everything"

--- solve.py
+++ solve.py
@@ -133,33 +133,35 @@ def can_fill(board, i, j, trans_p):
                     return False
     return True
 def recursive(board, remaining_orig, pents, transform):
-	if len(remaining_orig) == 0:
-		return ["solution:"]
+    if len(remaining_orig) == 0:
+        return ["solution:"]
 
 
     # if (check(board) == False):
     #     #print(board)
     #     return []
     # #print("remain", remaining_orig)
-	remaining = remaining_orig.copy()
+    remaining = remaining_orig.copy()
 
-	seen = set()
-	m = board.shape[0]
-	n = board.shape[1]
-	def area(r,c):
-		if not ( 0 <= r < m and 0 <= c < n and (r, c) not in seen and board[r][c] == 0):
-			return 0
-		seen.add((r,c))
-		return (1 + area(r+1, c) + area(r-1, c) +area(r, c-1) + area(r, c+1))
+    seen = set()
+    m = board.shape[0]
+    n = board.shape[1]
+    def area(r,c):
+        if not ( 0 <= r < m and 0 <= c < n and (r, c) not in seen and board[r][c] == 0):
+            return 0
+        seen.add((r,c))
+        return (1 + area(r+1, c) + area(r-1, c) +
+                    area(r, c-1) + area(r, c+1))
 
     # visited = np.zeros((m,n))
-	result  = m*n
-	for i in range(m):
-		for j in range(n):
-			if board[i][j] == 0 and (i,j) not in seen:
-				mianji = area(i, j)
-				if mianji % 5 != 0:
-					return []
+    result  = m*n
+    for j in range(n):
+        for i in range(m):
+        #for j in range(n):
+            if board[i][j] == 0 and (i,j) not in seen:
+                mianji = area(i, j)
+                if mianji % 5 != 0:
+                    return []
                 #elif mianji == 5:
                     # canFill = False
                     # for index in remaining:
@@ -175,52 +177,232 @@ def recursive(board, remaining_orig, pents, transform):
                     #                 return recur_result
                     # return []
 
-	print(board)
-	var = pents[remaining[0]]
-	var_idx = remaining[0]
-	tran_len = len(transform[var_idx])
-	for index in remaining:
-		trans_list = transform[index]
-		cur_len = len(trans_list)
-		if (cur_len <  tran_len):
-			tran_len = cur_len
-			var_idx = index
-	trans_list = transform[var_idx]
+    #print(board)
+    var = pents[remaining[0]]
+    var_idx = remaining[0]
+    tran_len = len(transform[var_idx])
+    for index in remaining:
+         trans_list = transform[index]
+         cur_len = len(trans_list)
+         if (cur_len <  tran_len):
+             tran_len = cur_len
+             var_idx = index
+    trans_list = transform[var_idx]
 
-	for i in range(m):
-		for j in range(n):
-			for var in trans_list:
-				if canput(board, i, j, var):
-					recur_board = board.copy()
-					put(recur_board, i, j, var)
-					recur_remain = remaining.copy()
-					recur_remain.remove(var_idx)
-					recur_result = recursive(recur_board, recur_remain, pents, transform)
-					if len(recur_result) != 0:
-						recur_result.append((var, (i,j)))
-						return recur_result
+    for i in range(m):
+        for j in range(n):
+            for var in trans_list:
+                if canput(board, i, j, var):
+                    recur_board = board.copy()
+                    put(recur_board, i, j, var)
+                    recur_remain = remaining.copy()
+                    recur_remain.remove(var_idx)
+                    recur_result = recursive(recur_board, recur_remain, pents, transform)
+                    if len(recur_result) != 0:
+                        recur_result.append((var, (i,j)))
+                        return recur_result
 
-	return []
+    return []
+def recursive_3(board, remaining_orig, pents, transform):
+    if len(remaining_orig) == 0:
+        return ["solution:"]
+
+
+    # if (check(board) == False):
+    #     #print(board)
+    #     return []
+    # #print("remain", remaining_orig)
+    remaining = remaining_orig.copy()
+    #print(board)
+    seen = set()
+    m = board.shape[0]
+    n = board.shape[1]
+    def area(r,c):
+        if not ( 0 <= r < m and 0 <= c < n and (r, c) not in seen and board[r][c] == 0):
+            return 0
+        seen.add((r,c))
+        return (1 + area(r+1, c) + area(r-1, c) +
+                    area(r, c-1) + area(r, c+1))
+
+    # visited = np.zeros((m,n))
+    result  = m*n
+    for j in range(n):
+        for i in range(m):
+        #for j in range(n):
+            if board[i][j] == 0 and (i,j) not in seen:
+                mianji = area(i, j)
+                if mianji % 3 != 0:
+                    return []
+    single = False
+    for trii in remaining_orig:
+        tri = pents[trii]
+        if tri.shape[0] == 1 or tri.shape[1] == 1 :
+            single = True
+    i = m - 2
+
+    if not single:
+        i = 0
+        for j in range(n - 1) :
+            if board[1][j] != 0 and board[1][j + 1] != 0 and board[0][j] == 0 and board[0][j + 1] == 0 :
+                return []
+        i = m - 1
+        for j in range(n - 1) :
+            if board[m - 2][j] != 0 and board[m - 2][j + 1] != 0 and board[i][j] == 0 and board[i][j + 1] == 0 :
+                return []
+        j = 0
+        for i in range(m - 1):
+            if board[i][1] != 0 and board[i + 1][1] != 0 and board[i][0] == 0 and board[i + 1][0] == 0 :
+                return []
+        j = n - 1
+        for i in range(m - 1):
+            if board[i][n - 2] != 0 and board[i + 1][n - 2] != 0 and board[i][j] == 0 and board[i + 1][j] == 0 :
+                return []
+        for i in range(m - 2):
+            for j in range(n - 2) :
+                if board[i + 2][j] != 0 and board[i + 2][j + 1] != 0 and board[i][j] != 0 and board[i][j + 1] != 0 and board[i + 1][j] == 0 and board[i + 1][j + 1] == 0 :
+                    return []
+                if board[i][j + 2] != 0 and board[i + 1][j + 2] != 0 and board[i][j] != 0 and board[i + 1][j] != 0 and board[i][j + 1] == 0 and board[i + 1][j + 1] == 0 :
+                    return []
+        #     j = n - 2
+        #     if board[i + 2][j] != 0 and board[i + 2][j + 1] != 0 and board[i][j] != 0 and board[i][j + 1] != 0 and board[i + 1][j] == 0 and board[i + 1][j + 1] == 0 :
+        #         return []
+        #     if board[i][j] != 0 and board[i + 1][j] != 0 and board[i][j + 1] == 0 and board[i + 1][j + 1] == 0 :
+        #         return []
+        # i = m - 2
+        # for j in range(n - 2) :
+        #     if  board[i][j] != 0 and board[i][j + 1] != 0 and board[i + 1][j] == 0 and board[i + 1][j + 1] == 0 :
+        #         return []
+        #     if board[i][j + 2] != 0 and board[i + 1][j + 2] != 0 and board[i][j] != 0 and board[i + 1][j] != 0 and board[i][j + 1] == 0 and board[i + 1][j + 1] == 0 :
+        #         return []
+
+
+
+    #print(board)
+    var = pents[remaining[0]]
+    var_idx = remaining[0]
+
+    trans_list = transform[var_idx]
+    #print("line 249")
+    for j in range(n):
+        for i in range(m):
+
+
+            for var in trans_list:
+                #print("line 254", var)
+                if canput(board, i, j, var):
+                    recur_board = board.copy()
+                    put(recur_board, i, j, var)
+                    recur_remain = remaining.copy()
+                    recur_remain.remove(var_idx)
+                    recur_result = recursive_3(recur_board, recur_remain, pents, transform)
+                    if len(recur_result) != 0:
+                        recur_result.append((var, (i,j)))
+                        return recur_result
+
+    return []
+def recursive_2(board, remaining_orig, pents, transform):
+    if len(remaining_orig) == 0:
+        return ["solution:"]
+
+
+    # if (check(board) == False):
+    #     #print(board)
+    #     return []
+    # #print("remain", remaining_orig)
+    remaining = remaining_orig.copy()
+
+    seen = set()
+    m = board.shape[0]
+    n = board.shape[1]
+    def area(r,c):
+        if not ( 0 <= r < m and 0 <= c < n and (r, c) not in seen and board[r][c] == 0):
+            return 0
+        seen.add((r,c))
+        return (1 + area(r+1, c) + area(r-1, c) +
+                    area(r, c-1) + area(r, c+1))
+
+    # visited = np.zeros((m,n))
+    # result  = m*n
+    # for j in range(n):
+    #     for i in range(m):
+    #     #for j in range(n):
+    #         if board[i][j] == 0 and (i,j) not in seen:
+    #             mianji = area(i, j)
+    #             if mianji % 5 != 0:
+    #                 return []
+    #             #elif mianji == 5:
+    #                 # canFill = False
+    #                 # for index in remaining:
+    #                 #     for trans_p in transform[index]:
+    #                 #         if (canput(board, i, j, trans_p)):
+    #                 #             print(board, "check")
+    #                 #             recur_board = board.copy()
+    #                 #             put(recur_board, i, j, trans_p)
+    #                 #             recur_remain = remaining.copy()
+    #                 #             recur_remain.remove(index)
+    #                 #             recur_result = recursive(recur_board, recur_remain, pents, transform)
+    #                 #             if len(recur_result) != 0:
+    #                 #                 return recur_result
+    #                 # return []
+    #
+    # #print(board)
+    var = pents[remaining[0]]
+    var_idx = remaining[0]
+    # tran_len = len(transform[var_idx])
+    # for index in remaining:
+    #      trans_list = transform[index]
+    #      cur_len = len(trans_list)
+    #      if (cur_len <  tran_len):
+    #          tran_len = cur_len
+    #          var_idx = index
+    trans_list = transform[var_idx]
+    #print(board)
+    for i in range(m):
+        for j in range(n):
+            for var in trans_list:
+                if canput(board, i, j, var):
+                    recur_board = board.copy()
+                    put(recur_board, i, j, var)
+                    recur_remain = remaining.copy()
+                    recur_remain.remove(var_idx)
+                    recur_result = recursive_2(recur_board, recur_remain, pents, transform)
+                    if len(recur_result) != 0:
+                        recur_result.append((var, (i,j)))
+                        return recur_result
+
+    return []
 def solve(board, pents):
 
     # m = board.shape[0]
     # n = board.shape[1]
-	sol_board = 1 - board
+    sol_board = 1 - board
 
     # create a dictonary to store all the transformations of each pentomino
-	transform = {}
-	for i, pentomino in enumerate(pents):
-		transform[i] = all_transformation(pentomino)
+    transform = {}
+    for i, pentomino in enumerate(pents):
+        transform[i] = all_transformation(pentomino)
     # for k, v in transform.items():
     #     print(k)
     #     print(v)
-	num_pents = len(pents)
-	remaining_orig = list(range(num_pents))
-	solution = recursive(sol_board, remaining_orig, pents, transform)
-	solution.pop(0)
-	print ("solution:", board)
-	print ("board size:", board.shape)
-	return solution
+    num_pents = len(pents)
+    tile = pents[0]
+    size = 0
+    for i in range(tile.shape[0]):
+        for j in range(tile.shape[1]):
+            if tile[i][j] != 0:
+                size += 1
+    print(size)
+    remaining_orig = list(range(num_pents))
+    if size == 5 :
+        solution = recursive(sol_board, remaining_orig, pents, transform)
+    elif size == 3:
+        solution = recursive_3(sol_board, remaining_orig, pents, transform)
+    elif size == 2:
+        solution = recursive_2(sol_board, remaining_orig, pents, transform)
+    solution.pop(0)
+    # print ("solution:", solution)
+    print ("board size:", board.shape)
+    return solution
 
 ##########      garbage
     # remaining_orig = list(range(num_pents))
